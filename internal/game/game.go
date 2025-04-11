d4bc803d5da6a042e8ca5959df3a50f2fb0b1cbf
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2025-04-11 02:22:44 +0200

    feat(player): simple player handling

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,21 +1,50 @@
 package game
 
 import (
+	"errors"
+
+	"github.com/N3moAhead/harvest/internal/component"
+	"github.com/N3moAhead/harvest/internal/player"
 	"github.com/N3moAhead/harvest/pkg/config"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 // --- Types ---
 
-type Game struct{}
+type Game struct {
+	Player *player.Player
+}
 
 func (g *Game) Update() error {
+	// --- Check for Exit ---
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		return errors.New("Game Quit!")
+	}
+
+	// --- Player Input & Movement ---
+	moveDir := component.Vector2D{X: 0, Y: 0}
+	if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyUp) {
+		moveDir.Y -= 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyDown) {
+		moveDir.Y += 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		moveDir.X -= 1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		moveDir.X += 1
+	}
+	if moveDir.Y != 0 && moveDir.X != 0 {
+		moveDir = moveDir.Normalize()
+	}
+	g.Player.Pos = g.Player.Pos.Add(moveDir.Mul(g.Player.Speed))
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	g.Player.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -33,6 +62,9 @@ func init() {
 // --- Public ---
 
 func NewGame() *Game {
-	g := &Game{}
+	p := player.NewPlayer()
+	g := &Game{
+		Player: p,
+	}
 	return g
 }
