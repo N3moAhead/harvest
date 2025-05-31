29c1a108b82adb624fa8139a2368bfeb0a5b3c2f
tom-as <tom@as.com>
2025-05-31 23:18:31 +0200

    feat(cookstation-spawn): Add random recipe generation and spawn cook stations in game scene

--- internal/scene/gamescene.go
+++ internal/scene/gamescene.go
@@ -218,21 +218,22 @@ func (g *GameScene) Update() error {
 
 	// --- World ---
 
-	// SPAWN ENEMIES, based on elapsed time
+	// SPAWN ENEMIES, based on elapsed time + SPAWN COOK STATIONS
 	elapsedMs := float64(time.Since(g.startTime).Milliseconds())
 	elapsedSec := elapsedMs / 1000.0
 	difficulty := 1.0 + math.Sqrt(elapsedSec)/10.0 // increase difficulty over time, use square root to make it slower at the beginning
 	// difficulty := 1.0 + elapsedSec/60.0 // 60.0 seconds is too short
 
 	// intervalSec := baseIntervalSec / difficulty                        // decrease spawning interval/duration based on difficulty/ time
-	intervalSec := config.BASE_SPAWN_INTERVAL_SEC / difficulty                  // decrease spawning interval/duration based on difficulty/ time
-	count := int(math.Ceil(float64(config.BASE_COUNT_PER_BATCH) * difficulty))  // increase count of batches based on difficulty (number of pools --> per pool multiple enemies)
-	mixProgress := util.Clamp((elapsedSec-config.MIX_START_SEC)/10.0, 0.0, 1.0) // mix progress from 0 to 1, after 120 seconds it will be 1.0
+	intervalSec := config.BASE_SPAWN_INTERVAL_SEC / difficulty                          // decrease spawning interval/duration based on difficulty/ time
+	enemyPoolCount := int(math.Ceil(float64(config.BASE_COUNT_PER_BATCH) * difficulty)) // increase count of batches based on difficulty (number of pools --> per pool multiple enemies)
+	mixProgress := util.Clamp((elapsedSec-config.MIX_START_SEC)/10.0, 0.0, 1.0)         // mix progress from 0 to 1, after 120 seconds it will be 1.0
 
 	// fmt.Printf("Elapsed Time: %.2f seconds, Mix Progress: %.2f, Interval: %.2f seconds, Count: %d, Difficulty: %.2f \n", elapsedSec, mixProgress, intervalSec, count, difficulty)
 	if time.Since(g.lastSpawnTime).Seconds() >= intervalSec {
 		g.lastSpawnTime = time.Now()
-		g.spawnBatch(count, mixProgress)
+		g.spawnEnemyBatch(enemyPoolCount, mixProgress)
+		g.spawnCookBatch(1) // TODO: maybe adjust number of cook stations
 	}
 
 	// --- Update World ---
@@ -332,7 +333,7 @@ func (g *GameScene) SetIsRunning(running bool) {
 	g.isRunning = running
 }
 
-func (g *GameScene) spawnBatch(count int, mixProgress float64) {
+func (g *GameScene) spawnEnemyBatch(count int, mixProgress float64) {
 	pool := make([]string, count)
 	for i := range pool {
 		pool[i] = enemy.RandomEnemyType().String()
@@ -350,8 +351,8 @@ func (g *GameScene) spawnBatch(count int, mixProgress float64) {
 	for _, t := range pool {
 		// fmt.Printf("Spawning %s enemy\n", t)
 		if mixProgress < 0.3 {
-			mapOffsetX, mapOffsetY := g.World.GetCameraPosition()
-			g.Enemies = append(g.Enemies, g.Spawner.SpawnRandomInView(t, mapOffsetX, mapOffsetY))
+			cameraX, cameraY := g.World.GetCameraPosition()
+			g.Enemies = append(g.Enemies, g.Spawner.SpawnRandomInView(t, cameraX, cameraY))
 		} else {
 			switch rand.Intn(3) {
 			case 0:
@@ -362,6 +363,16 @@ func (g *GameScene) spawnBatch(count int, mixProgress float64) {
 				g.Enemies = append(g.Enemies, g.Spawner.SpawnLine(t, g.Player.Pos, 5, 40, 10)...)
 			}
 		}
+	}
+}
+
+func (g *GameScene) spawnCookBatch(count int) {
+	for i := 0; i < count; i++ {
+		cameraX, cameraY := g.World.GetCameraPosition()
+		spawnX, spawnY := util.GetRandomPositionInView(cameraX, cameraY) // Get a random position in the view
+		recipe := cooking.GetRandomRecipe()
+		station := cooking.NewCookStation(spawnX, spawnY, recipe, 1.0) // TODO 1.0 is the cost factor, can be adjusted by elapsed time
+		g.cookStations = append(g.cookStations, station)
 	}
 }
 
